85ac48d1ba82ddc66f6ef9ae9a54d3566e2aeabe
git-dmitriy <shalberkind@mail.ru>
2021-12-16 22:51:05 +0300

    Create styles

--- src/App.vue
+++ src/App.vue
@@ -26,4 +26,34 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.app {
+  padding: 20px;
+}
+form {
+  margin-bottom: 20px;
+}
+
+.input-field {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid teal;
+  margin-bottom: 10px;
+}
+
+.btn-container {
+  display: flex;
+}
+
+.btn {
+  border: 1px solid teal;
+  padding: 10px 20px;
+  margin-left: auto;
+}
+
+.post {
+  padding: 15px;
+  border: 1px solid teal;
+}
+</style>
