8f4e4f5c06a3593b3144a4c76c804ceb922ac814
git-dmitriy <shalberkind@mail.ru>
2021-12-18 22:10:47 +0300

    Create pagination component

---
+++ src/components/ui/UiPagination.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="pagination">
+    <div
+      class="pagination-page"
+      v-for="pageNumber in totalPages"
+      :key="pageNumber"
+      :class="{ 'pagination-page-current': currentPage === pageNumber }"
+      @click="clickHandler(pageNumber)"
+    >
+      {{ pageNumber }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ui-pagination',
+  props: {
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    clickHandler(pageNumber) {
+      this.$emit('clickHandler', pageNumber);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pagination-page {
+  border: 2px solid gray;
+  padding: 10px;
+  margin: 0 4px;
+  cursor: pointer;
+}
+.pagination-page-current {
+  border: 2px solid teal;
+  color: teal;
+}
+</style>
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,5 +2,6 @@ import UiButton from '@/components/ui/UiButton';
 import UiInput from '@/components/ui/UiInput';
 import UiModal from '@/components/ui/UiModal';
 import UiSelect from '@/components/ui/UiSelect';
+import UiPagination from '@/components/ui/UiPagination';
 
-export default [UiButton, UiInput, UiModal, UiSelect];
+export default [UiButton, UiInput, UiModal, UiSelect, UiPagination];
