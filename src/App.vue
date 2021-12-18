49ca7e9333db4786d867f6e8ab987d59abfc7224
git-dmitriy <shalberkind@mail.ru>
2021-12-18 12:29:06 +0300

    Using watch for sorting posts

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,11 @@ export default {
   mounted() {
     this.fetchPosts();
   },
+  watch: {
+    selectedSort(newValue) {
+      this.posts.sort((a, b) => a[newValue]?.localeCompare(b[newValue]));
+    },
+  },
 };
 </script>
 
