ceb1b0a1cfa1b98e8b367503723947ef504ca760
git-dmitriy <shalberkind@mail.ru>
2021-12-18 13:58:05 +0300

    Create animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length !== 0">
     <h1>Публикации:</h1>
-    <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h1 v-else>Не доступных публикаций</h1>
 </template>
@@ -32,5 +34,23 @@ export default {
 .post {
   padding: 15px;
   border: 1px solid teal;
+  margin-bottom: 10px;
+}
+
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.post-list-move {
+  transition: transform 0.3s ease;
 }
 </style>
