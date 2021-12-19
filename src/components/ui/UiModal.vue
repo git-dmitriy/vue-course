e9279e82469e034c343a73d6394e61ad7e3df20a
git-dmitriy <shalberkind@mail.ru>
2021-12-20 00:34:19 +0300

    Using mixins

--- src/components/ui/UiModal.vue
+++ src/components/ui/UiModal.vue
@@ -7,19 +7,11 @@
 </template>
 
 <script>
+import toggleMixin from '@/mixins/toggleMixin';
+
 export default {
   name: 'ui-modal',
-  props: {
-    show: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  methods: {
-    closeModal() {
-      this.$emit('update:show', false);
-    },
-  },
+  mixins: [toggleMixin],
 };
 </script>
 
