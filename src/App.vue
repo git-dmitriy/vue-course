ed85c23e440afe9c33df3b52e9f4a52c8288703f
git-dmitriy <shalberkind@mail.ru>
2021-12-19 16:28:00 +0300

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app">
+    <ui-navigation />
     <router-view />
   </div>
 </template>
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,5 +3,13 @@ import UiInput from '@/components/ui/UiInput';
 import UiModal from '@/components/ui/UiModal';
 import UiSelect from '@/components/ui/UiSelect';
 import UiPagination from '@/components/ui/UiPagination';
+import UiNavigation from '@/components/ui/UiNavigation';
 
-export default [UiButton, UiInput, UiModal, UiSelect, UiPagination];
+export default [
+  UiButton,
+  UiInput,
+  UiModal,
+  UiSelect,
+  UiPagination,
+  UiNavigation,
+];
